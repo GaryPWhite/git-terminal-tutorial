0d8d83807cf9eb17c04c290c2a95f94229237447
Gary P White <garypwhite@live.com>
2019-04-09 20:43:09 -0400

    Updates

---
+++ precache-manifest.40539537eefb14294c352f2800c7eeee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d80e298cd3dcf81723b",
+    "url": "/git-trainer/static/css/main.ec9a01c0.chunk.css"
+  },
+  {
+    "revision": "8d80e298cd3dcf81723b",
+    "url": "/git-trainer/static/js/main.c6b0dd45.chunk.js"
+  },
+  {
+    "revision": "c56cf168d10cabbb984b",
+    "url": "/git-trainer/static/js/runtime~main.fbad5b65.js"
+  },
+  {
+    "revision": "1b958cfde07bb85b0060",
+    "url": "/git-trainer/static/js/2.8e6b728b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-trainer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9453aaa3ce6f17f1cb25f8b5b317b6a1",
+    "url": "/git-trainer/index.html"
+  }
+];
